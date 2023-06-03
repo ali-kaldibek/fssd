cabadd4115f027d112ce84a7b18965c6889e500d
Ali <131841612+ali-kaldibek@users.noreply.github.com>
2023-06-03 20:27:10 +0600

    Delete index.js

--- js/css/index.js
+++
@@ -1,7 +0,0 @@
-let accordionItem = document.getElementsByClassName('accordion__item');
-
-for (let i =0; i < accordionItem.length; i++){
-accordionItem[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-    });
-}
